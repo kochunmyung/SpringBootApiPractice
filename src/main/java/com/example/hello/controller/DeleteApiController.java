daa39a219df0724f58cdd7a27b96bf8319b7e882
kochunmyung <rhcjsaud1@naver.com>
2024-01-23 06:26:15 +0900

    DELETE API Practice

---
+++ src/main/java/com/example/hello/controller/DeleteApiController.java
@@ -0,0 +1,13 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/deleteapi")
+public class DeleteApiController {
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+        System.out.println(userId);
+        System.out.println(account);
+    }
+}
